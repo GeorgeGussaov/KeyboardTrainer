07e9c6416de6d914629e22230b4fe7d1b0adfd53
gussa <gussa@DESKTOP-QFC5O7V>
2023-05-02 19:09:51 +0300

    Добавьте файлы проекта.

---
+++ KeyboardTrainer/FormMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace KeyboardTrainer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelText = new Label();
+            textBoxTrainingField = new TextBox();
+            labelCntError = new Label();
+            labelCurrentWord = new Label();
+            SuspendLayout();
+            // 
+            // labelText
+            // 
+            labelText.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            labelText.Location = new Point(12, 9);
+            labelText.Name = "labelText";
+            labelText.Size = new Size(776, 171);
+            labelText.TabIndex = 0;
+            labelText.Text = "Text from file";
+            // 
+            // textBoxTrainingField
+            // 
+            textBoxTrainingField.Location = new Point(315, 258);
+            textBoxTrainingField.Name = "textBoxTrainingField";
+            textBoxTrainingField.Size = new Size(166, 23);
+            textBoxTrainingField.TabIndex = 1;
+            textBoxTrainingField.TextChanged += textBoxTrainingField_TextChanged;
+            // 
+            // labelCntError
+            // 
+            labelCntError.AutoSize = true;
+            labelCntError.Location = new Point(12, 372);
+            labelCntError.Name = "labelCntError";
+            labelCntError.Size = new Size(85, 15);
+            labelCntError.TabIndex = 2;
+            labelCntError.Text = "Count errors: 0";
+            // 
+            // labelCurrentWord
+            // 
+            labelCurrentWord.AutoSize = true;
+            labelCurrentWord.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelCurrentWord.Location = new Point(315, 366);
+            labelCurrentWord.Name = "labelCurrentWord";
+            labelCurrentWord.Size = new Size(106, 21);
+            labelCurrentWord.TabIndex = 3;
+            labelCurrentWord.Text = "Current word:";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(labelCurrentWord);
+            Controls.Add(labelCntError);
+            Controls.Add(textBoxTrainingField);
+            Controls.Add(labelText);
+            Name = "FormMain";
+            Text = "Keyboard Trainer";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelText;
+        private TextBox textBoxTrainingField;
+        private Label labelCntError;
+        private Label labelCurrentWord;
+    }
+}
---
+++ KeyboardTrainer/FormMain.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics;
+
+namespace KeyboardTrainer
+{
+    //1 - Крестный отец
+    //2 - Хранители снов
+    //3 - Великолепная семерка
+    //4 - Ведьмак 2
+    //5 - Хоббит: Неожиданное путешествие
+    //6 - Назад в будущее
+    //7 - Бэтмен: Начало
+    //8 - Джентельмены
+    //9 - Бешеные псы
+    //10 - Во все тяжкие
+    public partial class FormMain : Form
+    {
+        int check = 0;
+        int cntErr = 0;
+        string title = "";
+        public FormMain()
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+            int ind = rnd.Next(0, 10);
+            string[] text = { "text1.txt", "text2.txt", "text3.txt", "text4.txt", "text5.txt", "text6.txt", "text7.txt", "text8.txt", "text9.txt", "text10.txt" };
+            string[] ans = { "Крестный отец", "Хранители снов", "Великолепная семерка", "Ведьмак 2: Убийца королей", "Хоббит: Неожиданное путешествие", "Назад в будущее", "Бэтмен: Начало", "Джентельмены", "Бешеные псы", "Во все тяжкие"};
+            StreamReader sr = new StreamReader(text[ind]);
+            title = ans[ind];
+            labelText.Text = sr.ReadToEnd();
+            sr.Close();
+            labelCurrentWord.Text = "Current word: " + labelText.Text.Split()[0];
+
+
+        }
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        private void textBoxTrainingField_TextChanged(object sender, EventArgs e)
+        {
+            string[] s = labelText.Text.Split(new string[] {" " ,"\n", "\r\n"}, StringSplitOptions.RemoveEmptyEntries);//нагло взято с инета, в душе ни чаю что это, но оно работает
+            //string text = labelText.Text;
+            if (textBoxTrainingField.Text.Split().Length > 1) //опустошаем строку после введенного слова, попутно проверяя
+            {
+                if (s[check] + " " == textBoxTrainingField.Text) //проверка слова на совпадение в тексте
+                    check++;
+                else
+                {
+                    cntErr++;                  //подсчет ошибок
+                    labelCntError.Text = "Count errors: " + cntErr;
+                }
+                textBoxTrainingField.Text = "";
+                if (check == s.Length)
+                {
+                    sw.Stop();
+                    MessageBox.Show("Текст из: " + title + "\nКоличество ошибок: " + cntErr + "\nВремени затрачено: " + sw.Elapsed + "\nСреднее кол-во символов в минуту: " + Convert.ToDouble(labelText.Text.Length)/(Convert.ToDouble(sw.ElapsedMilliseconds)/60000));
+                    this.Close();
+                }
+                labelCurrentWord.Text = "Current word: " + s[check];
+            }
+
+        }
+
+    }
+}
